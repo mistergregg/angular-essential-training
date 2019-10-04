0c89deba81e6e5bf1479b48b65898cc23fec50b1
Gregory <itsgregreed@gmail.com>
2019-10-04 09:29:15 -0400

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { InterpolationConfig } from '@angular/compiler';
+import { getCurrencySymbol } from '@angular/common';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
